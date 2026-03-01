67a633aaf0f4d34de8c5a3e60347563116531af6
scdonnelly <scdonnelly@scu.edu>
2026-03-01 01:56:55 -0800

    Sara commit 1
    Setting up the server.js and changing the posfile.jsx a little

--- myapp/app/profile.jsx
+++ myapp/app/profile.jsx
@@ -1,16 +1,25 @@
 import { Text, View } from "react-native";
 import { common } from "../src/Styles/common";
 import { profile } from "../src/Styles/profile";
+import { useEffect, useState } from "react";
+
+const API_URL = "http://localhost:8080";
 
 
 export default function Profile() {
+    const [profileData, setProfileData] = useState(null);
+    useEffect(() => {
+    fetch(`${API_URL}/profile/testuser123`)
+        .then(res => res.json())
+        .then(data => setProfileData(data));
+    }, []);
     return (
         <View style={common.screen}>
             <View style={profile.profile_top}>
                 <View style={profile.circle} />
-                <Text style={profile.name}>Name</Text>
-                <Text style={profile.info}>John Doe</Text>
-                <Text style={profile.info}>Freshman</Text>
+                <Text style={profile.info}>{profileData?.name || "John Doe"}</Text>
+                <Text style={profile.info}>{profileData?.year || "Freshman"}</Text>
+                <Text style={profile.info}>{profileData?.major || "Major"}</Text>
             </View>
             <Text style={profile.name}>Requirements</Text>
             <View style={profile.cardContainer}>
